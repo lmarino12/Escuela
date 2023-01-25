665615bf35f4070862d3d6e24878b8427987ea5b
lmarino12 <lmarino@est.ups.edu.ec>
2023-01-25 13:53:33 -0500

    avance

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -9,8 +9,17 @@ def chao(request):
 
 def registro(request):
     alumnos = []
+    alumno = []
     nombre = input("Ingrese su nombre: ")
     cedula = int(input("Ingrese su numero de cedula: "))
     edad = int(input("Ingrese su edad: "))
     celular = int(input("Ingrese su numero de telefono: "))
     correo = input("Ingrese su correo: ")
+    alumno.append(nombre)
+    alumno.append(cedula)
+    alumno.append(edad)
+    alumno.append(celular)
+    alumno.append(correo)
+    alumnos.append(alumno)
+    alumno.clear()
+    return HttpResponse(alumnos[0])

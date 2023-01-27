952040c6f3f0410fe372d159f9d33fdc2d3916e3
lmarino12 <lmarino@est.ups.edu.ec>
2023-01-27 17:00:57 -0500

    avance

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, JsonResponse
 from .models import *
 from django.shortcuts import get_object_or_404, render, redirect
 from .forms import CreateStudent
+from django import forms
 # Create your views here.
 def hola(request):
     return render(request, "index.html")
@@ -13,9 +14,13 @@ def registro(request):
             "form": CreateStudent()
         })
     else:
-        Alumno.objects.create(email=request.POST["email"], f_name=request.POST["f_name"],
-                              l_name=request.POST["l_name"], password=request.POST["password"],
-                              username=request.POST["username"])
+        for alum in Alumno.objects.all():
+            if alum.username == request.POST["username"]:
+                return ("/horario/")
+            else:
+                Alumno.objects.create(email=request.POST["email"], f_name=request.POST["f_name"],
+                                    l_name=request.POST["l_name"], password=request.POST["password"],
+                                    username=request.POST["username"])
         return redirect("/")
 
 

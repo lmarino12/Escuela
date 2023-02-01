12f3a8789c1a8c1beba4463b74df4f7746c5b7d1
lmarino12 <lmarino@est.ups.edu.ec>
2023-01-31 20:02:17 -0500

    pruebas unitarias

--- test/test_alumno.py
+++ test/test_alumno.py
@@ -10,4 +10,14 @@ def test_user_creation():
         email = "asasd",
         password = "asasd"
     )
-    assert user.id == 1
+    assert user.username == "asasd"
+
+def test_prof_creation():
+    user=Maestro.objects.create_user(
+        username="asasd",
+        f_name = "asasd",
+        l_name = "asasd",
+        email = "asasd",
+        password = "asasd"
+    )
+    assert user.username == "asasd"
